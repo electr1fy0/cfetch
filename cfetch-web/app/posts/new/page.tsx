772364cf872ee38d4e532e9b10606a8a0c0d9c4a
electr1fy0 <electr1fy0@icloud.com>
2026-02-14 22:45:53 +0530

    prepare cfetch web, setup auth, prisma and landing design

---
+++ cfetch-web/app/posts/new/page.tsx
@@ -0,0 +1,6 @@
+import Form from "next/form";
+import prisma from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+export default function NewPost() {}
