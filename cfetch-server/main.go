7d24cbe9eeac929c866e9b7e8f2fe80369eea8ef
electr1fy0 <electr1fy0@icloud.com>
2026-02-19 21:13:15 +0530

    add problem submission

--- cfetch-server/main.go
+++ cfetch-server/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"sync"
@@ -52,6 +55,44 @@ func (h *Handler) CreateRoom(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%s", room.ID)
 }
 
+type SubmissionResponse struct {
+	Stdout        *string `json:"stdout"`
+	Time          string  `json:"time"`
+	Memory        int     `json:"memory"`
+	Stderr        *string `json:"stderr"`
+	Token         string  `json:"token"`
+	CompileOutput *string `json:"compile_output"`
+	Message       *string `json:"message"`
+	Status        Status  `json:"status"`
+}
+
+type Status struct {
+	ID          int    `json:"id"`
+	Description string `json:"description"`
+}
+
+// {
+//   "source_code": "int main(){return 0;}",
+//   "language_id": 54,
+//   "stdin": "input here",
+//   "expected_output": "expected output",
+//   "cpu_time_limit": 2,
+//   "memory_limit": 128000,
+//   "wall_time_limit": 5,
+//   "redirect_stderr_to_stdout": true
+// }
+
+type SubmissionRequest struct {
+	SourceCode             string `json:"source_code"`
+	LanguageID             int    `json:"language_id"`
+	Stdin                  string `json:"stdin"`
+	ExpectedOutput         string `json:"expected_output"`
+	CPUTimeLimit           int    `json:"cpu_time_limit"`
+	MemoryLimit            int    `json:"memory_limit"`
+	WallTimeLimit          int    `json:"wall_time_limit"`
+	RedirectStderrToStdout bool   `json:"redirect_stderr_to_stdout"`
+}
+
 func (h *Handler) JoinRoom(w http.ResponseWriter, r *http.Request) {
 	username := r.URL.Query().Get("username")
 	roomID, _ := uuid.Parse(r.URL.Query().Get("room-id"))
@@ -73,15 +114,54 @@ func (h *Handler) JoinRoom(w http.ResponseWriter, r *http.Request) {
 
 	defer c.Close(websocket.StatusNormalClosure, "")
 
-	var v any
+	var p SubmissionRequest
+	client := &http.Client{}
+	var judgeURL = "http://localhost:2358/submissions"
 	for {
-		err = wsjson.Read(r.Context(), c, &v)
+		err = wsjson.Read(r.Context(), c, &p)
+		if err != nil {
+			return
+		}
+		rawBytes, err := json.Marshal(p)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		log.Println("recieved:", v)
+		req, err := http.NewRequest("POST", judgeURL, bytes.NewReader(rawBytes))
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		req.Header.Add("Content-Type", "application/json")
+		resp, err := client.Do(req)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		var token Token
+		json.NewDecoder(resp.Body).Decode(&token)
+		go func(Token string) {
+			for {
+				resp, err := http.Get(judgeURL + "/" + Token)
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+
+				rawResp, err := io.ReadAll(resp.Body)
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+				fmt.Println(string(rawResp))
+			}
+		}(token.Token)
 	}
+}
+
+type Token struct {
+	Token string `json:"token"`
 }
 
 func main() {
